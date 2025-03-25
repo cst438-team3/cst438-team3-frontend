3802ec9a9b6687e8069fb3b677dbdf89622a947a
Taylor Beimel <tbeimel@csumb.edu>
2025-03-24 20:15:58 -0700

    Implemented AssignmentGrade.js

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import InstructorHome from './components/instructor/InstructorHome';
 import AssignmentsView from './components/instructor/AssignmentsView';
 import EnrollmentsView from './components/instructor/EnrollmentsView';
 import InstructorSectionsView from './components/instructor/InstructorSectionsView';
+import AssignmentGrade from './components/instructor/AssignmentGrade';
 
 
 function App() {
@@ -65,6 +66,7 @@ function App() {
               <Route path="assignments" element={<AssignmentsView />} />
               <Route path="enrollments" element={<EnrollmentsView />} />
               <Route path="sections" element={<InstructorSectionsView />} />
+              <Route path="/assignments/:assignmentId/grades" element={<AssignmentGrade />} />
             </Route>
           </Routes>
         </BrowserRouter>
