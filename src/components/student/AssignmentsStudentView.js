44983364a7ab5581daa0263e907f7cfc5c68e1ee
Jose Garcia <jmgarcia0202@gmail.com>
2025-03-25 10:45:47 -0700

    Completed AssignmentsStudentView

--- src/components/student/AssignmentsStudentView.js
+++ src/components/student/AssignmentsStudentView.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react';
+import {SERVER_URL} from "../../Constants";
+import Button from "@mui/material/Button";
 
 // student views a list of assignments and assignment grades 
 // use the URL  /assignments?studentId= &year= &semester=
@@ -8,11 +10,67 @@ import React, {useState} from 'react';
 // display a table with columns  Course Id, Assignment Title, Assignment DueDate, Score
 
 const AssignmentsStudentView = (props) => {
-    
-     
+    const [term, setTerm] = useState({year:'', semester:''});
+    const [assignments, setAssignments] = useState([])
+    const [message, setMessage] = useState('');
+
+    const headers = ['Assignment Id', 'Title', 'Due Date'];
+    const onChange = (event) => {
+        setTerm({...term, [event.target.name]:event.target.value});
+    }
+
+    const fetchAssignments = async () => {
+        try {
+            const response = await fetch(`${SERVER_URL}/assignments?studentId=3&year=${term.year}&semester=${term.semester}`)
+            if (response.ok) {
+                const data = await response.json();
+                setAssignments(data)
+            } else {
+                const rc = await response.json()
+                setMessage("Response error: " + rc.message)
+            }
+        } catch (err) {
+            setMessage("Network error: " + err.message);
+        }
+    }
     return(
-        <> 
-            <h3>Not implemented</h3>   
+        <>
+            <h3>Enter Information</h3>
+            <table className="Center">
+                <tbody>
+                <tr>
+                    <td>Year:</td>
+                    <td><input type="text" id="year" name="year" value={term.year} onChange={onChange} /></td>
+                </tr>
+                <tr>
+                    <td>Semester:</td>
+                    <td><input type="text" id="semester" name="semester" value={term.semester} onChange={onChange} /></td>
+                </tr>
+                </tbody>
+            </table>
+
+            <Button onClick={fetchAssignments}> View Assignments </Button>
+
+            <div>
+                <h3>Assignments</h3>
+                <h4>{message}</h4>
+                <table className="Center">
+                    <thead>
+                    <tr>
+                        {headers.map((s, idx) => (<th key={idx}>{s}</th>))}
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {assignments.map((a) => (
+                        <tr key={a.assignmentId}>
+                            <td>{a.assignmentId}</td>
+                            <td>{a.title}</td>
+                            <td>{a.dueDate}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
         </>
     );
 }
